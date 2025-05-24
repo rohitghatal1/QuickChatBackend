343300f57d84c2e5ef5f79be0ec1de33989d4593
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-05-24 16:10:05 +0545

    added register auth controller

---
+++ controllers/authController.ts
@@ -0,0 +1,27 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User";
+import {generateToken} from "../utils/jwt"
+
+
+export const registerUser = async (req:any, res:any) => {
+    try{
+        const {name, number, username, email, password} = req.body;
+
+        const userExists = await User.findOne({number});
+        if(userExists) return res.status(400).json({status: "Failed", message: "This number already used"});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({name, number, username, email, password: hashedPassword});
+
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            number: user.number,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user.id),
+        })
+    } catch(err:any){
+        res.status(500).json({message: "Server error"})
+    }
+};
