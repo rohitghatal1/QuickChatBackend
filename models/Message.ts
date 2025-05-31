768959c3ff9737bac4fc75edccb6d70782506ce2
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-05-31 15:29:45 +0545

    updated message schema

--- models/Message.ts
+++ models/Message.ts
@@ -1,13 +1,40 @@
 import mongoose, {Document, Schema} from "mongoose";
+import {encrypt, decrypt} from '../utils/encryption';
+import { timeStamp } from "console";
 
-const messageSchema = new mongoose.Schema(
-    {
-        sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-        receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-        content: {type: String, required: true},
-        timestamp: {type: Date, default: Date.now},
-        read: {type: Boolean, default: false},
+interface IMessage extends Document{
+    sender: mongoose.Types.ObjectId;
+    receiver: mongoose.Types.ObjectId;
+    content: string;
+    encryptedContent: string;
+    timestamp: Date;
+    read: boolean;
+}
+
+const MessageSchema: Schema = new Schema ({
+    sender: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    receiver: {type: Schema.Types.ObjectId, ref:'User', required: true},
+    content: { type: String, select: false},
+    encryptedContent: {type: String, required: true},
+    timeStamp: {type: Date, default: Date.now},
+    read: {type: Boolean, default: false}
+});
+
+// encryption before saving
+MessageSchema.pre<IMessage>('save', function (next) {
+    if(this.isModified('content')){
+        this.encryptedContent = encrypt(this.content);
+        this.content = "";
     }
-);
+    next();
+});
 
-export default mongoose.model("Message", messageSchema);
+MessageSchema.post<IMessage>('find', function(docs:any){
+    docs.forEach((doc:any) => {
+        if(doc.encryptedContent){
+            doc.content = decrypt(doc.encryptedContent)
+        }
+    });
+});
+
+export default mongoose.model<IMessage>('Message', MessageSchema)
