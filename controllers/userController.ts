843ea31b29f9351c63a702f6a1935295915c3559
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-04 20:33:20 +0545

    updated

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -20,6 +20,14 @@ export const getUserById = async (req:any, res:any) => {
         res.status(200).json(user);
 
     } catch(err:any){
-        res.status(500).json({message: "Server Errro"}, err)
+        res.status(500).json({message: "Server Error"}, err)
+    }
+}
+
+export const changePassword = async (req:any, res:any) => {
+    try{
+
+    } catch(err:any){
+        res.status(500).json({message: "Server Error"})
     }
 }
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUserById, getUsers } from '../controllers/userController';
+import { changePassword, getUserById, getUsers } from '../controllers/userController';
 
 const router = express.Router();
 
 router.get("/getUsers", getUsers);
 router.get("/getUsers/:id", getUserById);
+router.patch('/user/changePassword', changePassword);
 
 export default router
