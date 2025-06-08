9e478054144fdd3447d324b393315237245a4595
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-08 21:05:52 +0545

    added change password code

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcryptjs"
 
 export const getUsers = async (req:any, res:any) => {
     try{
@@ -27,6 +28,24 @@ export const getUserById = async (req:any, res:any) => {
 
 export const changePassword = async (req:any, res:any) => {
     try{
+        const {currentPassword, newPassword} = req.body;
+        const userId = req.user.id;
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json({status: 'failed', message: 'User not found'});
+            
+        }
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
+        if(!isMatch){
+            return res.status(400).json({status: 'failed', message: 'Current password is incorrect'}) 
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+
+        res.status(200).json({status: "success", message: 'Password changed successfully'})
 
     } catch(err:any){
         res.status(500).json({status: "failed", message: "Server Error"})
