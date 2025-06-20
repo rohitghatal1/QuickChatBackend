0e84cc51f36ebe848cf2c248507ded70faa56e21
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-20 21:19:48 +0545

    updated send message function

--- controllers/chatController.ts
+++ controllers/chatController.ts
@@ -1,3 +1,4 @@
+import ChatRoom from "../models/ChatRoom";
 import Message from "../models/Message";
 import User from "../models/User";
 
@@ -39,30 +40,26 @@ export const getMyMessages = async (req: any, res: any) => {
 
 
 export const sendMessage = async (req:any, res:any) => {
-    try{
-        const {receiverId, content} = req.body;
-        const senderId = req.user.id;
+    const {roomId, content} = req.body;
+    const senderId = req.user.id;
 
-        if(!receiverId || !content){
-            return res.status(400).json({message: "Missing required fields"});
-        }
-
-        const message = await Message.create({
-            sender: senderId,
-            receiver: receiverId,
-            content
-        })
-
-        const populatedMessage = await Message.findById(message.id)
-        .populate('sender', 'username')
-        .populate('receiver', 'username')
-
-        res.json(populatedMessage)
-
-    } catch(err:any){
-        console.error("Error sending  message:", err.message);
-        res.status(500).json({message: err.message})
+    if(!roomId || !content){
+        return res.status(400).json({status: "failed", message: "Missing fields"})
     }
+
+    const message = await Message.create({
+        chatRoom: roomId,
+        sender: senderId,
+        content
+    });
+
+    await ChatRoom.findByIdAndUpdate(roomId, {lastMessage: message._id});
+
+    const populatedMessage = await Message.findById(message._id)
+    .populate('sender', 'username')
+    .populate('chatRoom');
+
+    res.json(populatedMessage);
 }
 
 
