9c5c429bf6442ea0232ae2c931614382c29e497a
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-21 16:12:26 +0545

    updated

--- controllers/chatController.ts
+++ controllers/chatController.ts
@@ -1,95 +1,123 @@
+import path from "path";
 import ChatRoom from "../models/ChatRoom";
 import Message from "../models/Message";
 import User from "../models/User";
+import { populate } from "dotenv";
 
-export const getOrCreateRoom = async (req:any, res:any) => {
-    try{
-        const userId = req.user.id;
-        const {receiverId} = req.body;
+export const getOrCreateRoom = async (req: any, res: any) => {
+  try {
+    const userId = req.user.id;
+    const { receiverId } = req.body;
 
-        let room = await ChatRoom.findOne({
-            isGroup: false,
-            participants: {$all: [userId, receiverId], $size: 2},
-        });
+    let room = await ChatRoom.findOne({
+      isGroup: false,
+      participants: { $all: [userId, receiverId], $size: 2 },
+    });
 
-        if(!room){
-            room = await ChatRoom.create({
-                isGroup: false,
-                participants: [userId, receiverId],
-            });
-        }
-
-        res.json(room);
-    } catch(err:any){
-        res.status(500).json({status: "failed", message: "Failed to get or create room", error: err.message})
+    if (!room) {
+      room = await ChatRoom.create({
+        isGroup: false,
+        participants: [userId, receiverId],
+      });
     }
-}
+
+    res.json(room);
+  } catch (err: any) {
+    res.status(500).json({
+      status: "failed",
+      message: "Failed to get or create room",
+      error: err.message,
+    });
+  }
+};
 
 export const getMyChatRooms = async (req: any, res: any) => {
-    try {
-        const userId = req.user.id;
+  try {
+    const userId = req.user.id;
 
-        const chatRooms = await ChatRoom.find({
-            participants: userId
-        })
-        .populate({
-            path: 'participants',
-            select: 'username name email' 
-        })
-        .populate({
-            path: 'lastMessage',
-            populate: {
-                path: 'sender',
-                select: 'username'
-            }
-        })
-        .sort({ updatedAt: -1 });
+    const chatRooms = await ChatRoom.find({
+      participants: userId,
+    })
+      .populate({
+        path: "participants",
+        select: "username name email",
+      })
+      .populate({
+        path: "lastMessage",
+        populate: {
+          path: "sender",
+          select: "username",
+        },
+      })
+      .sort({ updatedAt: -1 });
 
-        res.status(200).json(chatRooms);
-    } catch (err: any) {
-        console.error('Error fetching chat rooms: ', err.message);
-        res.status(500).json({ 
-            status: 'failed', 
-            message: 'Failed to fetch chat rooms', 
-            error: err.message 
-        });
-    }
+    res.status(200).json(chatRooms);
+  } catch (err: any) {
+    console.error("Error fetching chat rooms: ", err.message);
+    res.status(500).json({
+      status: "failed",
+      message: "Failed to fetch chat rooms",
+      error: err.message,
+    });
+  }
 };
 
-export const getRoomMessages = async (req:any, res:any) => {
-    try{
-        const {roomId} = req.params;
+export const getRoomMessages = async (req: any, res: any) => {
+  try {
+    const { roomId } = req.params;
 
-        const messages = await Message.find({room: roomId})
-        .sort({createdAt: -1})
-        .populate('sender', 'username name email')
-        .populate('receiver', 'username name email');
+    console.log("room messges fetched with id: ", roomId);
+    const messages = await Message.find({ chatRoom: roomId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "sender",
+        select: "username name email",
+      })
+      .lean();
 
-        res.status(200).json(messages);
-    } catch(err:any){
-        console.error('Error fetching messages: ', err.message);
-        res.status(500).json({status: 'failed', message: 'Server error'});
-    }
+    const formattedMessage = messages.map((msg: any) => ({
+      _id: msg._id,
+      chatRoom: msg.chatRoom,
+      sender: {
+        id: msg.sender._id,
+        username: msg.sender.username,
+        name: msg.sender.name,
+        email: msg.sender.email,
+      },
+      content: msg.content,
+      readBy: msg.readBy || [],
+      createdAt: msg.createdAt,
+      updatedAt: msg.updatedAt,
+    }));
+
+    res.status(200).json(formattedMessage);
+    console.log("response sent with: ", messages);
+  } catch (err: any) {
+    console.error("Error fetching messages: ", err.message);
+    res.status(500).json({ status: "failed", message: "Server error" });
+  }
 };
 
-export const getMessages = async (req:any, res:any) => {
-    try{
-        const { userId } = req.params;
-        const currentUserId = req.user.id;
-        console.log("user id: ", req.user)
+export const getMessages = async (req: any, res: any) => {
+  try {
+    const { userId } = req.params;
+    const currentUserId = req.user.id;
+    console.log("user id: ", req.user);
 
-        const messages = await Message.find({
-            $or: [
-                { sender: currentUserId, receiver: userId },
-                { sender: userId, receiver: currentUserId }
-            ]
-        }).sort('timestamp').populate('sender receiver', 'username');
+    const messages = await Message.find({
+      $or: [
+        { sender: currentUserId, receiver: userId },
+        { sender: userId, receiver: currentUserId },
+      ],
+    })
+      .sort("timestamp")
+      .populate("sender receiver", "username");
 
-        res.json(messages);
-    } catch(err:any){
-        res.status(500).json({message: err.message});
-    }
-}
+    res.json(messages);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 export const getMyMessages = async (req: any, res: any) => {
   try {
@@ -98,46 +126,63 @@ export const getMyMessages = async (req: any, res: any) => {
       .populate("receiver", "username");
 
     if (chats.length === 0) {
-      return res.status(404).json({ status: "failed", message: "No chats found" });
+      return res
+        .status(404)
+        .json({ status: "failed", message: "No chats found" });
     }
 
     return res.status(200).json(chats);
   } catch (err: any) {
     console.log("Failed to get chats: ", err);
-    return res.status(500).json({ status: "error", message: "Internal Server Error" });
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal Server Error" });
   }
 };
 
+export const sendMessage = async (req: any, res: any) => {
+  const { roomId, content } = req.body;
+  const senderId = req.user.id;
 
-export const sendMessage = async (req:any, res:any) => {
-    const {roomId, content} = req.body;
-    const senderId = req.user.id;
+  if (!roomId || !content) {
+    return res
+      .status(400)
+      .json({ status: "failed", message: "Missing fields" });
+  }
+  console.log(
+    "send message called with room id and content: ",
+    roomId,
+    content
+  );
 
-    if(!roomId || !content){
-        return res.status(400).json({status: "failed", message: "Missing fields"})
-    }
+  const message = await Message.create({
+    chatRoom: roomId,
+    sender: senderId,
+    content,
+  });
 
-    const message = await Message.create({
-        chatRoom: roomId,
-        sender: senderId,
-        content
+  await ChatRoom.findByIdAndUpdate(roomId, { lastMessage: message._id });
+
+  const populatedMessage = await Message.find({ _id: message._id })
+    .populate("sender", "username")
+    .populate({
+      path: "chatRoom",
+      populate: {
+        path: "lastMessage",
+        populate: { path: "sender", select: "username" },
+      },
     });
 
-    await ChatRoom.findByIdAndUpdate(roomId, {lastMessage: message._id});
+  res.json(populatedMessage[0]);
+};
 
-    const populatedMessage = await Message.findById(message._id)
-    .populate('sender', 'username')
-    .populate('chatRoom');
-
-    res.json(populatedMessage);
-}
-
-
-export const getUsers = async (req:any, res:any) =>{
-    try{
-        const users = await User.find({_id: {$ne: req.user._id}}).select('-password');
-        res.json(users);
-    } catch(err:any){
-        res.status(500).json({message: err.message});
-    }
-}
+export const getUsers = async (req: any, res: any) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.user._id } }).select(
+      "-password"
+    );
+    res.json(users);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- models/Message.ts
+++ models/Message.ts
@@ -1,46 +1,22 @@
-import mongoose,{Document, mongo, Schema} from "mongoose";;
+import mongoose, { Document, mongo, Schema } from "mongoose";
 import { encrypt, decrypt } from "../utils/encryption";
 import { ref } from "process";
 
-interface IMessage extends Document{
-    chatRoom: mongoose.Types.ObjectId;
-    sender: mongoose.Types.ObjectId;
-    content?: string;
-    encryptedContent: string;
-    readBy: mongoose.Types.ObjectId[];
+interface IMessage extends Document {
+  chatRoom: mongoose.Types.ObjectId;
+  sender: mongoose.Types.ObjectId;
+  content?: string;
+  readBy: mongoose.Types.ObjectId[];
 }
 
 const MessageSchema = new Schema<IMessage>(
-    {
-        chatRoom: {type: Schema.Types.ObjectId, ref: 'ChatRoom', required: true},
-        sender: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-        content: {type: String, select: false},
-        encryptedContent: {type: String, required: true},
-        readBy: [{type: Schema.Types.ObjectId, ref: 'User'}]
-    },
-    {timestamps: true}
+  {
+    chatRoom: { type: Schema.Types.ObjectId, ref: "ChatRoom", required: true },
+    sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    readBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
 );
 
-MessageSchema.pre<IMessage>('save', function (next){
-    if(this.isModified('content') && this.content) {
-        this.encryptedContent = encrypt(this.content);
-        this.content = undefined;
-    }
-    next();
-});
-
-MessageSchema.post<IMessage>('find', function (docs:any) {
-    docs.forEach((doc:any) => {
-        if(doc.encryptedContent){
-            doc.content = decrypt(doc.encryptedContent);
-        }
-    })
-});
-
-MessageSchema.post<IMessage>('findOne', function (doc:any) {
-    if(doc?.encryptedContent){
-        doc.content = decrypt(doc.encryptedContent);
-    }
-})
-
-export default mongoose.model<IMessage>('Message', MessageSchema);
+export default mongoose.model<IMessage>("Message", MessageSchema);
