abae52ac5e4f20d50fc469d6ad7e253b2393e063
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-23 22:12:52 +0545

    added admin routes

--- admin/routes/authRoutes.ts
+++ admin/routes/authRoutes.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import { verifyToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
+router.post("/register", verifyToken)
+router.post("/login", verifyToken );
+
+
+export default router
