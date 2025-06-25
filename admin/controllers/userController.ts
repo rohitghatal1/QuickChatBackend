0ad9298b9cd42e8a4e88770ccffc3bca785a23e9
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-25 21:50:01 +0545

    added user getter in admin

---
+++ admin/controllers/userController.ts
@@ -0,0 +1,16 @@
+import User from "../../models/User"
+
+export const getAllUsers = async (req:any, res:any) => {
+    try{
+        const users = await User.find();
+
+        if(!users){
+            return res.status(404).json({status: "failed", message: "No users found" });
+        }
+
+        res.staus(200).json(users)
+    } catch (err){
+        console.log("Error fetching users: ", err)
+        res.status(400).json({status: "failed", message: "Server error!"})
+    }
+}
---
+++ admin/routes/userRoutes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { getAllUsers } from "../controllers/userController";
+
+const router = express.Router();
+
+router.get("/getUsers", getAllUsers);
+router.get("/getMessages", );
