03c8cd2a3ad48bca4b190db288aa4f32327f9044
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-26 18:45:44 +0545

    added delete message contrller

--- admin/controllers/messageControllers.ts
+++ admin/controllers/messageControllers.ts
@@ -1,23 +1,37 @@
-import Message from "../../models/Message"
+import Message from "../../models/Message";
 
-export const getMessages = async (req:any, res:any) => {
-    try{
-        const messages = await Message.find();
+export const getMessages = async (req: any, res: any) => {
+  try {
+    const messages = await Message.find();
 
-        if(!messages){
-            return res.status(404).json({status: "failed", message: "No messages found"})
-        }
-
-        return res.status(200).json(messages);
-    } catch (err:any){
-        res.status(500).json({status: "Failed", message: "Serve error"})
+    if (!messages) {
+      return res
+        .status(404)
+        .json({ status: "failed", message: "No messages found" });
     }
-}
 
-export const deleteMessage = async (req:any, res:any) => {
-    try{
+    return res.status(200).json(messages);
+  } catch (err: any) {
+    res.status(500).json({ status: "Failed", message: "Serve error" });
+  }
+};
 
-    } catch (err:any){
-        res.status(404).json({status: "failed", message: "Server Error"})
+export const deleteMessage = async (req: any, res: any) => {
+  try {
+    const { messaageId } = req.body;
+
+    if (!messaageId) {
+      return res
+        .status(400)
+        .json({ status: "failed", message: "User Id required" });
     }
-}
+
+    Message.findByIdAndDelete(messaageId);
+
+    res.json({
+      status: "success",
+    });
+  } catch (err: any) {
+    res.status(404).json({ status: "failed", message: "Server Error" });
+  }
+};
