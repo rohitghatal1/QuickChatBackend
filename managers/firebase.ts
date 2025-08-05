9c4a58dbb2eaba44aa857fade7a5748f6b2cdbcc
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-08-05 21:26:28 +0545

    updated

--- managers/firebase.ts
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin";
-import serviceAccount from "./service-account.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
-
-export default admin;
